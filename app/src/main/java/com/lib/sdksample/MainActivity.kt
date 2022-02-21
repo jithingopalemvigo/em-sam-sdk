1b441dc94252ed73e5684f2b65c82b3e4a3497db
jithingopal <jithingopal@emvigotech.com>
2022-02-21 10:02:30 +0530

    added implementation, calling code classes working well

--- app/src/main/java/com/lib/sdksample/MainActivity.kt
+++ app/src/main/java/com/lib/sdksample/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.lib.sdksample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.begini.androidsdkv2.ui.base.BaseActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btnStartSdk).setOnClickListener{
+            startActivity(Intent(this,BaseActivity::class.java))
+        }
     }
 }
